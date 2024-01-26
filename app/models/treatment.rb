89fafa62a40aaefe4939ecfcbce2a24eeedb2029
onjatiana <onjatiana.ramanitrarivo@gmail.com>
2024-01-26 21:38:31 +0300

    fix: modification de la validation de l'entity treatment

--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -2,9 +2,7 @@ class Treatment < ApplicationRecord
   belongs_to :category
 
   validates :name, presence: true
-  validates :translation, presence: true
-  validates :state, presence: true
-
+  
   has_many :treatment_lines, dependent: :destroy
   
 end
