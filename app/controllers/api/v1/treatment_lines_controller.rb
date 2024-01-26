598fa6f5f71480c52cfab7e52a52adcf37b65d8e
onjatiana <onjatiana.ramanitrarivo@gmail.com>
2024-01-26 11:18:16 +0300

    feat: mise en place de create, findAll, find de l'entity Treatment_lines

--- app/controllers/api/v1/treatment_lines_controller.rb
+++ app/controllers/api/v1/treatment_lines_controller.rb
@@ -1,16 +1,27 @@
 class Api::V1::TreatmentLinesController < ApplicationController
+  
   def create
-    render json:{message:"mande ny create"}
+    treatment_line = TreatmentLine.new(treatment_line_params)
+    if treatment_line.save
+      render json: {treatment_line: treatment_line}, status: :created
+    else 
+      render json: {errors: treatment_line.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def index
-    render json:{message:"mande ny index"}
-
+    render json: {treatment_lines: TreatmentLine.where(treatment_id:params[:treatment_id])}
   end
 
   def index_all
-    render json:{message:"mande ny index_all"}
+    render json: {treatment_lines: TreatmentLine.all}
 
   end
-  
+
+  private 
+
+  def treatment_line_params 
+    params.require(:treatment_line).permit(:date, :patientName, :state, :treatment_id) 
+  end
+
 end
--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -1,6 +1,10 @@
 class Treatment < ApplicationRecord
   belongs_to :category
 
+  validates :name, presence: true
+  validates :translation, presence: true
+  validates :state, presence: true
+
   has_many :treatment_lines, dependent: :destroy
   
 end
