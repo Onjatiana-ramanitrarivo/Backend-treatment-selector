b18c674615fcb6fc4b12bd4c7e2604a1de44b767
onjatiana <onjatiana.ramanitrarivo@gmail.com>
2024-01-30 14:06:06 +0300

    feat: Ajout des données initiales dans la base de données

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,23 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+diagnostics = Category.create(title: 'Diagnostics')
+examinations = Category.create(title: 'Examinations')
+
+Treatment.create([
+
+    {name: 'angiocardiography',translation: '',state: true, category: diagnostics},
+    {name: 'angiography',translation: '',state: true, category: diagnostics},
+    {name: 'brain scanning',translation: '',state: true, category: diagnostics},
+
+    {name: 'auscultation',translation: '',state: true, category: examinations},
+    {name: 'autopsy',translation: '',state: true, category: examinations},
+    {name: 'biopsy',translation: '',state: true, category: examinations},
+    
+])
+
+cholecystography = Treatment.create(name: 'cholecystography',translation: '',state: true, category: diagnostics)
+bronchoscopy = Treatment.create(name: 'bronchoscopy',translation: '',state: true, category: examinations)
+
+TreatmentLine.create([
+    {date: '2024-01-30 09:41:09',patientName: 'Ramanitrarivo Onjatiana',state: 'pending',treatment: cholecystography},
+    {date: '2024-01-30 07:30:34',patientName: 'Rasoamanana Mickaela',state: 'pending',treatment: bronchoscopy}
+])
